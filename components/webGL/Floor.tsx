6aba4ba2a71fd886e6102d662ba08d64a4342f9b
lauhengyi <lauhengyi@gmail.com>
2023-04-17 00:23:11 +0800

    Shifted Floor

--- components/webGL/Floor.tsx
+++ components/webGL/Floor.tsx
@@ -1,6 +1,6 @@
 export default function Floor() {
   return (
-    <mesh rotation-x={-Math.PI * 0.5} position-y={-0.01}>
+    <mesh rotation-x={-Math.PI * 0.5} position={[0, -0.01, -40]}>
       <planeGeometry args={[100, 100]} />
       <meshBasicMaterial color={'#c7d774'} />
     </mesh>
