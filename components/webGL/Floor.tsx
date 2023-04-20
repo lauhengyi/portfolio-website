46b2794eb5ac7b60cd61cc333dc3979b9792af4d
lauhengyi <lauhengyi@gmail.com>
2023-04-20 13:54:39 +0800

    Recentered floor

--- components/webGL/Floor.tsx
+++ components/webGL/Floor.tsx
@@ -1,6 +1,6 @@
 export default function Floor() {
   return (
-    <mesh rotation-x={-Math.PI * 0.5} position={[0, -0.01, -40]}>
+    <mesh rotation-x={-Math.PI * 0.5} position={[0, -0.01, 0]}>
       <planeGeometry args={[100, 100]} />
       <meshBasicMaterial color={'#c7d774'} />
     </mesh>
