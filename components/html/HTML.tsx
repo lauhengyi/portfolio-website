5ceae2d409a4151aba203d2ae554ab4f49c2505d
lauhengyi <lauhengyi@gmail.com>
2023-04-22 14:23:45 +0800

    Added Basic html template for landing

--- components/html/HTML.tsx
+++ components/html/HTML.tsx
@@ -1,16 +1,29 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
+import NavBar from './NavBar';
 
 export default function HTML() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Lau Heng Yi</title>
         <meta name="description" content="Lau Heng Yi's portfolio website" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anek+Kannada&family=Bebas+Neue&family=DM+Serif+Text&family=Dongle&family=Kurale&family=Lily+Script+One&family=Oswald&family=Pattaya&family=Playfair+Display&family=Rampart+One&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <main>
-        <h1 className={styles.title}>Hi</h1>
+      <main className={styles.container}>
+        <NavBar />
+        <article className={styles.landing}>
+          <div className={styles.overview}>
+            <h1 className={styles.title}>Web Developer</h1>
+            <p className={styles.caption}>I like to build nice things.</p>
+          </div>
+        </article>
       </main>
     </div>
   );
---
+++ components/html/NavBar.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <ul>
+      <li>
+        <Link href={'/'}>Home</Link>
+      </li>
+    </ul>
+  );
+}
