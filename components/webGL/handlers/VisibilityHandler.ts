76cd6b2aedc646eb288b362a0171152b5a321458
lauhengyi <lauhengyi@gmail.com>
2023-04-26 19:24:23 +0800

    Removed deadcode

--- components/webGL/handlers/VisibilityHandler.ts
+++ components/webGL/handlers/VisibilityHandler.ts
@@ -13,13 +13,11 @@ export default class VisibilityHandler {
   }
 
   handleVisibility() {
-    console.log('handleVisibility');
     const phases = getPhasePositions();
     useFrame(() => {
       const progress =
         phases.landing.get() + phases.sky.get() + phases.space.get();
       if (progress < 1) {
-        console.log(this.sceneRefs.landing.current);
         this.sceneRefs.landing.current.visible = true;
       } else if (progress < 2) {
         this.sceneRefs.landing.current.visible = false;
