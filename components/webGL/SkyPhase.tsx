eea468d60f69e82d94a358002137a1c00d0003f3
lauhengyi <lauhengyi@gmail.com>
2023-05-04 23:15:53 +0800

    Added more trajectories

--- components/webGL/SkyPhase.tsx
+++ components/webGL/SkyPhase.tsx
@@ -1,11 +1,13 @@
 import { forwardRef } from 'react';
 import AboutMeText from './skyPhase/AboutMeText';
+import Planes from './skyPhase/Planes';
 
 const LandingPhase = forwardRef<THREE.Group>(
   (props: JSX.IntrinsicElements['group'], ref) => {
     return (
       <group {...props} ref={ref}>
         <AboutMeText />
+        <Planes />
       </group>
     );
   },
