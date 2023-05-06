a7f9228ec22d6d54818a42aa71d8f37518b86011
lauhengyi <lauhengyi@gmail.com>
2023-05-06 18:22:48 +0800

    Made caption animation smoother

--- components/html/sky/SkyCaption.tsx
+++ components/html/sky/SkyCaption.tsx
@@ -1,5 +1,5 @@
 import styles from '../../../styles/Sky.module.css';
-import { motion, MotionValue, useTransform } from 'framer-motion';
+import { motion, MotionValue, useTransform, useSpring } from 'framer-motion';
 
 type props = {
   children: string;
@@ -7,12 +7,12 @@ type props = {
 };
 
 export default function SkyCaption({ children, skyProgress }: props) {
-  const opacity = useTransform(skyProgress, [0.8, 1], [0, 1]);
+  const opacity = useSpring(useTransform(skyProgress, [0.8, 1], [0, 1]), {
+    bounce: 0,
+  });
   return (
-    <>
-      <motion.p style={{ opacity: opacity }} className={styles.caption}>
-        {children}
-      </motion.p>
-    </>
+    <motion.p style={{ opacity: opacity }} className={styles.caption}>
+      {children}
+    </motion.p>
   );
 }
