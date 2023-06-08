6e1c7ad85435ef45d1c054a80176fa01cd4a3f77
lauhengyi <lauhengyi@gmail.com>
2023-06-08 22:38:36 +0800

    Added buffer space for mobile, due to scrolling issue

--- components/utils/phasePositions.ts
+++ components/utils/phasePositions.ts
@@ -1,3 +1,5 @@
-const phasePos: number[] = [0, 0.1, 0.3, 0.4, 0.9, 1];
+import { isMobile } from 'react-device-detect';
+
+const phasePos: number[] = [0, 0.1, 0.3, 0.4, 0.9, isMobile ? 0.98 : 1];
 
 export default phasePos;
